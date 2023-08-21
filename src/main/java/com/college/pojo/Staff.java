bb5a9f03a6565ec5a0b0cb6ee56ad10e680738c4
Pratiksha Bhokare <pratikshabhokare283@gmail.com>
2023-08-21 21:49:05 +0530

    new class is added into project

---
+++ src/main/java/com/college/pojo/Staff.java
@@ -0,0 +1,141 @@
+package com.college.pojo;
+
+public class Staff {
+
+	private int staffId;
+	private int departmentId;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String gender;
+	private double salary;
+	private String position;
+	private String qualification;
+	private String experience;
+	private int contactNumber;
+	private String emailId;
+	private Address addressId;
+	private boolean isExist;
+
+	public int getStaffId() {
+		return staffId;
+	}
+
+	public void setStaffId(int staffId) {
+		this.staffId = staffId;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+	public String getExperience() {
+		return experience;
+	}
+
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+
+	public int getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(int contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Address getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Address addressId) {
+		this.addressId = addressId;
+	}
+
+	public boolean isExist() {
+		return isExist;
+	}
+
+	public void setExist(boolean isExist) {
+		this.isExist = isExist;
+	}
+
+	@Override
+	public String toString() {
+		return "Staff [staffId=" + staffId + ", departmentId=" + departmentId + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", age=" + age + ", gender=" + gender + ", salary=" + salary
+				+ ", position=" + position + ", qualification=" + qualification + ", experience=" + experience
+				+ ", contactNumber=" + contactNumber + ", emailId=" + emailId + ", addressId=" + addressId
+				+ ", isExist=" + isExist + "]";
+	}
+
+}
