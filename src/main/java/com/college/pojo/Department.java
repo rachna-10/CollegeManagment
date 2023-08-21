06eae1edbaa3dcf5728755d4097a06ad022283b5
rachna madankar <madankarrachna@gmail.com>
2023-08-21 19:24:08 +0530

    Structure is Created.

---
+++ src/main/java/com/college/pojo/Department.java
@@ -0,0 +1,14 @@
+package com.college.pojo;
+
+public class Department {
+	private int id;
+	private String name;
+    private int numberOfStaff;
+	private int numberOfStudent;
+	private int studId;
+	private int clgId;
+	private int courseId;
+	private int isEnable;
+
+
+}
---
+++ src/main/java/com/college/pojo/Student.java
@@ -0,0 +1,20 @@
+package com.college.pojo;
+
+public class Student {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int rollNo;
+	private String sem;
+    private int attends;
+	private String subject;
+	private int phoneNo;
+	private int aadhrNo;
+	private int marks;
+	private int isEnable;
+	private int clgId;
+	private int deptId;
+	private String courseId;
+	private int addId;
+}
