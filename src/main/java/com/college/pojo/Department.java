a152dffb326965c4ccfa3610bcb646b842c7fa3f
shruti dahule <shrutidahule88@gmail.com>
2023-08-22 14:50:35 +0530

    created getter setter in Department class

--- src/main/java/com/college/pojo/Department.java
+++ src/main/java/com/college/pojo/Department.java
@@ -3,12 +3,75 @@ package com.college.pojo;
 public class Department {
 	private int id;
 	private String name;
-    private int numberOfStaff;
+	private int numberOfStaff;
 	private int numberOfStudent;
 	private int studId;
 	private int clgId;
 	private int courseId;
 	private int isEnable;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumberOfStaff() {
+		return numberOfStaff;
+	}
+
+	public void setNumberOfStaff(int numberOfStaff) {
+		this.numberOfStaff = numberOfStaff;
+	}
+
+	public int getNumberOfStudent() {
+		return numberOfStudent;
+	}
+
+	public void setNumberOfStudent(int numberOfStudent) {
+		this.numberOfStudent = numberOfStudent;
+	}
+
+	public int getStudId() {
+		return studId;
+	}
+
+	public void setStudId(int studId) {
+		this.studId = studId;
+	}
+
+	public int getClgId() {
+		return clgId;
+	}
+
+	public void setClgId(int clgId) {
+		this.clgId = clgId;
+	}
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public int getIsEnable() {
+		return isEnable;
+	}
+
+	public void setIsEnable(int isEnable) {
+		this.isEnable = isEnable;
+	}
 
 }
